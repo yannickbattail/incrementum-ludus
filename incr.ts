6d752c2becf67cc34234c1bd75ce1f0c7cb65235
Yannick Battail <yannick.battail@gmail.com>
2019-03-26 17:29:00 +0000

    fix typo copPer

--- incr.ts
+++ incr.ts
@@ -1,16 +1,20 @@
 
 const IRON = new Resource("iron");
-const COPER = new Resource("coper");
+const COPPER = new Resource("coper");
 const LEAD = new Resource("lead");
 
 var engine = new Engine();
 engine.Player = new Player("platypus");
 engine.Sources = [
-    new TimedSource("iron mine", new ResourceQuantity(IRON, 2), 500),
-    new TimedSource("coper mine", new ResourceQuantity(COPER, 1), 3000)
+    // initals sources
+    new TimedSource("iron mine", new ResourceQuantity(IRON, 2), 500/*mili sec*/),
+    new TimedSource("copper mine", new ResourceQuantity(COPPER, 1), 3000)
 ];
 engine.Triggers = [
-    new Trigger("lead mine exploitation", [new ResourceQuantity(IRON, 20), new ResourceQuantity(COPER, 2)],
-        new TimedSource("lead mine", new ResourceQuantity(LEAD, 1), 5000)/*, [], []*/)
+    new Trigger("lead mine exploitation",
+        // trigger when reach these resources quantity
+        [new ResourceQuantity(IRON, 20), new ResourceQuantity(COPPER, 2)],
+        // and then spwan source
+        new TimedSource("lead mine", new ResourceQuantity(LEAD, 1), 5000))
 ];
 
