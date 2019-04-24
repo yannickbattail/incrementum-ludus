439d8f2a01103282c00b227c17e4d64567104b34
Yannick Battail <yannick.battail@raisepartner.com>
2019-04-24 11:46:51 +0200

    remove domDiff

--- DesertIsland/DesertIslandGui.js
+++ DesertIsland/DesertIslandGui.js
@@ -202,12 +202,45 @@ var DesertIslandGui = (function () {
     DesertIslandGui.prototype.fastMode = function () {
         engine.FastMode = 1000;
     };
+    DesertIslandGui.prototype.hasChanged = function (node1, node2) {
+        if (node1.nodeType !== node2.nodeType)
+            return true;
+        if (node1.nodeType == Node.TEXT_NODE && node1.textContent !== node2.textContent)
+            return true;
+        return false;
+    };
+    DesertIslandGui.prototype.updateChildNodes = function (oldNode, newNode) {
+        var newNodeLength = newNode.childNodes.length;
+        var oldNodeLength = oldNode.childNodes.length;
+        var maxLength = Math.max(newNodeLength, oldNodeLength);
+        for (var i = 0; i < maxLength; i++) {
+            if (i >= oldNodeLength) {
+                try {
+                    oldNode.appendChild(newNode.childNodes[oldNodeLength]);
+                }
+                catch (e) {
+                    console.log(e);
+                }
+            }
+            else if (i >= newNodeLength) {
+                oldNode.removeChild(oldNode.childNodes[newNodeLength]);
+            }
+            else {
+                if (this.hasChanged(oldNode.childNodes[i], newNode.childNodes[i])) {
+                    oldNode.replaceChild(newNode.childNodes[i], oldNode.childNodes[i]);
+                }
+                else {
+                    this.updateChildNodes(oldNode.childNodes[i], newNode.childNodes[i]);
+                }
+            }
+        }
+    };
     DesertIslandGui.prototype.updateDiv = function (id, html) {
         var oldDiv = document.getElementById(id);
         if (oldDiv != null) {
             var newdiv = document.createElement('div');
             newdiv.innerHTML = html;
-            updateChildNodes(oldDiv, newdiv);
+            this.updateChildNodes(oldDiv, newdiv);
         }
     };
     DesertIslandGui.prototype.updateGui = function () {
--- DesertIsland/DesertIslandGui.ts
+++ DesertIsland/DesertIslandGui.ts
@@ -6,7 +6,6 @@
 /// <reference path="../Engine/interfaces/IPlayer.ts" />
 /// <reference path="../Engine/Engine.ts" />
 
-/// <reference path="./domDiff.ts" />
 /// <reference path="./Material.ts" />
 /// <reference path="./Item.ts" />
 /// <reference path="./Level.ts" />
@@ -235,12 +234,45 @@ class DesertIslandGui {
         engine.FastMode=1000;
     }
 
+
+    public hasChanged(node1 : Node, node2 : Node) {
+        if (node1.nodeType !== node2.nodeType)
+            return true;
+        if (node1.nodeType == Node.TEXT_NODE && node1.textContent !== node2.textContent) 
+            return true;
+
+        return false;
+    }
+      
+    public updateChildNodes(oldNode : Node, newNode : Node) {
+        let newNodeLength = newNode.childNodes.length
+        let oldNodeLength = oldNode.childNodes.length;
+        let maxLength = Math.max(newNodeLength, oldNodeLength);
+        for (let i = 0; i < maxLength; i++) {
+            if (i >= oldNodeLength) {
+                try {
+                    oldNode.appendChild(newNode.childNodes[oldNodeLength]);
+                } catch (e) {
+                    console.log(e);
+                }
+            } else if (i >= newNodeLength) {
+                oldNode.removeChild(oldNode.childNodes[newNodeLength]);
+            } else {
+                if (this.hasChanged(oldNode.childNodes[i], newNode.childNodes[i])) {
+                    oldNode.replaceChild(newNode.childNodes[i], oldNode.childNodes[i]);
+                } else {
+                    this.updateChildNodes(oldNode.childNodes[i], newNode.childNodes[i]);
+                }
+            }
+        }
+    }
+
     private updateDiv(id : string, html : string) {
         var oldDiv = document.getElementById(id);
         if (oldDiv != null) {
             var newdiv = document.createElement('div');
             newdiv.innerHTML = html;
-            updateChildNodes(oldDiv, newdiv);
+            this.updateChildNodes(oldDiv, newdiv);
         }
     }
 
