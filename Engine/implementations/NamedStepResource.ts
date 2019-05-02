fc19b3a25573919eaf2054394f3749854cad977b
Yannick Battail <yannick.battail@raisepartner.com>
2019-05-02 19:26:38 +0200

    add the Resource: NamedStepResource in the engine

---
+++ Engine/implementations/NamedStepResource.ts
@@ -0,0 +1,25 @@
+/// <reference path="../interfaces/IResource.ts" />
+/// <reference path="../interfaces/IQuantity.ts" />
+/// <reference path="../interfaces/IProducer.ts" />
+/// <reference path="../interfaces/ITrigger.ts" />
+/// <reference path="../interfaces/ICrafter.ts" />
+/// <reference path="../interfaces/IPlayer.ts" />
+/// <reference path="../Engine.ts" />
+/// <reference path="./Resource.ts" />
+
+class NamedStepResource extends Resource {
+    public $type : string = 'Item';
+    constructor(public name : string, public image : string, public stepNames : string[]){
+        super(name);
+    }
+    public static load(data : any) : NamedStepResource {
+        let r : NamedStepResource = new NamedStepResource(data.name, data.image, data.stepNames);
+        return r;
+    }
+    public show(quantity : number) : string {
+        if (quantity < 0 || quantity >= this.stepNames.length) {
+            return "" + quantity + ": UNKOWN";
+        }
+        return "" + quantity + ": " + this.stepNames[quantity];
+    }
+}
