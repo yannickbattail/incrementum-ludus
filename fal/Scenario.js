fab9f69ec315bf6b40ac01a8a0e41a0e74a97706
Yannick Battail <yannick.battail@gmail.com>
2019-05-05 16:39:19 +0000

    add pin's of multiple filieres

--- fal/Scenario.js
+++ fal/Scenario.js
@@ -9,11 +9,25 @@ var LEVEL = new NamedStepResource("level", "level.svg", [
     "Faluchard"
 ]);
 var TEMPS = new Item("temps", "time.png");
-var PINS_FILIERE = new Item("pin's filière", "pins_gris.png");
-var PINS_VILLE = new Item("pin's ville", "pins.png");
 var PARRAIN = new Item("parrain", "food.svg");
 var CODE_VILLE = new Item("code de ville", "etoile_or.png");
 var FALUCHE = new Item("faluche", "faluche.png");
+var PINS_INGE = new Item("pin's ingé", "pins_gris.png");
+var PINS_SCIENCES = new Item("pin's sciences", "pins_gris.png");
+var PINS_ROSE = new Item("pin's rose", "pins_gris.png");
+var PINS_DROIT = new Item("pin's droit", "pins_gris.png");
+var PINS_SAGE_POUF = new Item("pin's sage-pouf", "pins_gris.png");
+var PINS_JAUNE = new Item("pin's jaune", "pins_gris.png");
+var PINS_PHARMA = new Item("pin's Pharma", "pins_gris.png");
+var PINS_MEDECINE = new Item("pin's Médecine", "pins_gris.png");
+var PINS_LYON = new Item("pin's Lyon", "pins.png");
+var PINS_GRENOBLE = new Item("pin's Grenoble", "pins.png");
+var PINS_MONPEUL = new Item("pin's Monpeul", "pins.png");
+var PINS_VALENCE = new Item("pin's Valence", "pins.png");
+var PINS_MARSEILLE = new Item("pin's Marseille", "pins.png");
+var PINS_DIJON = new Item("pin's Dijon", "pins.png");
+var PINS_CLERMONT = new Item("pin's Clermont", "pins.png");
+var PINS_NANCY = new Item("pin's Nancy", "pins.png");
 var Scenario = (function () {
     function Scenario() {
     }
@@ -29,45 +43,81 @@ var Scenario = (function () {
         ];
         engine.crafters = [
             new Crafter("Apéro potes")
-                .thatCraft(new RandomRangeQuantity(0, 2, PINS_FILIERE))["in"](2).seconds()
+                .thatCraft(new RandomRangeQuantity(0, 2, PINS_INGE))["in"](2).seconds()
                 .atCostOf(Q(1, TEMPS))
         ];
         engine.triggers = [
             new Trigger("Sympatisant")
-                .whenReached(Q(5, PINS_FILIERE))
+                .whenReached(Q(5, PINS_INGE))
                 .spawnCrafter(new Crafter("Apéro filière")
-                .thatCraft(new RandomRangeQuantity(2, 5, PINS_FILIERE))["in"](2).seconds()
+                .thatCraft(new RandomRangeQuantity(2, 5, PINS_INGE))
+                .andCraft(new RandomResource(1, PINS_SCIENCES, 0.02))["in"](2).seconds()
                 .atCostOf(Q(1, TEMPS)))
                 .spawnResource(Q(1, LEVEL))
                 .appendTrigger(new Trigger("Impétrent")
-                .whenReached(Q(50, PINS_FILIERE))
+                .whenReached(Q(50, PINS_INGE))
                 .spawnCrafter(new Crafter("Apéro fal de ville")
-                .thatCraft(new RandomRangeQuantity(1, 3, PINS_VILLE))["in"](3).seconds()
+                .thatCraft(new RandomResource(1, PINS_INGE, 0.3))
+                .thatCraft(new RandomResource(1, PINS_SCIENCES, 0.3))
+                .thatCraft(new RandomResource(1, PINS_ROSE, 0.3))
+                .thatCraft(new RandomResource(1, PINS_DROIT, 0.3))
+                .thatCraft(new RandomResource(1, PINS_SAGE_POUF, 0.3))
+                .thatCraft(new RandomResource(1, PINS_JAUNE, 0.3))
+                .thatCraft(new RandomResource(1, PINS_PHARMA, 0.3))
+                .thatCraft(new RandomResource(1, PINS_MEDECINE, 0.3))["in"](3).seconds()
                 .atCostOf(Q(2, TEMPS)))
                 .spawnResource(Q(1, LEVEL))
                 .appendTrigger(new Trigger("Néo")
-                .whenReached(Q(20, PINS_VILLE))
+                .whenReached(Q(1, PINS_INGE))
+                .whenReached(Q(1, PINS_SCIENCES))
+                .whenReached(Q(1, PINS_ROSE))
+                .whenReached(Q(1, PINS_DROIT))
+                .whenReached(Q(1, PINS_SAGE_POUF))
+                .whenReached(Q(1, PINS_JAUNE))
+                .whenReached(Q(1, PINS_PHARMA))
+                .whenReached(Q(1, PINS_MEDECINE))
                 .spawnCrafter(new Crafter("Apéro fal hebdomadaire")
-                .thatCraft(Q(3, PINS_VILLE))["in"](3).seconds()
+                .thatCraft(new RandomResource(1, PINS_INGE, 0.4))
+                .thatCraft(new RandomResource(1, PINS_SCIENCES, 0.4))
+                .thatCraft(new RandomResource(1, PINS_DROIT, 0.4))
+                .thatCraft(new RandomResource(1, PINS_JAUNE, 0.4))
+                .thatCraft(new RandomResource(1, PINS_ROSE, 0.3))
+                .thatCraft(new RandomResource(1, PINS_SAGE_POUF, 0.3))
+                .thatCraft(new RandomResource(1, PINS_PHARMA, 0.3))
+                .thatCraft(new RandomResource(1, PINS_MEDECINE, 0.3))["in"](3).seconds()
                 .atCostOf(Q(2, TEMPS))
                 .canBeSwitchedToAuto())
                 .spawnCrafter(new Crafter("Trouver des parrains marraines")
                 .thatCraft(Q(1, PARRAIN))["in"](20).seconds()
-                .atCostOf(Q(30, PINS_VILLE)).and(Q(30, PINS_FILIERE)))
+                .atCostOf(Q(8, PINS_INGE))
+                .atCostOf(Q(6, PINS_SCIENCES))
+                .atCostOf(Q(6, PINS_DROIT))
+                .atCostOf(Q(6, PINS_JAUNE))
+                .atCostOf(Q(6, PINS_ROSE))
+                .atCostOf(Q(6, PINS_SAGE_POUF))
+                .atCostOf(Q(6, PINS_PHARMA))
+                .atCostOf(Q(6, PINS_MEDECINE)))
                 .spawnResource(Q(1, LEVEL))
                 .appendTrigger(new Trigger("Parrainé")
                 .whenReached(Q(2, PARRAIN))
                 .spawnCrafter(new Crafter("Apprentissage du code")
                 .thatCraft(Q(1, CODE_VILLE))["in"](30).seconds()
-                .atCostOf(Q(20, PINS_VILLE)))
+                .atCostOf(Q(4, PINS_INGE))
+                .atCostOf(Q(4, PINS_SCIENCES))
+                .atCostOf(Q(4, PINS_DROIT))
+                .atCostOf(Q(4, PINS_JAUNE))
+                .atCostOf(Q(4, PINS_ROSE))
+                .atCostOf(Q(4, PINS_SAGE_POUF))
+                .atCostOf(Q(4, PINS_PHARMA))
+                .atCostOf(Q(4, PINS_MEDECINE)))
                 .spawnResource(Q(1, LEVEL))
                 .appendTrigger(new Trigger("Baptisable")
                 .whenReached(Q(1, CODE_VILLE))
                 .spawnCrafter(new Crafter("Baptême")
-                .thatCraft(Q(1, FALUCHE))["in"](30).seconds()
+                .thatCraft(Q(1, FALUCHE)).andCraft(Q(1, PINS_GRENOBLE))["in"](30).seconds()
                 .atCostOf(Q(1, CODE_VILLE)).and(Q(2, PARRAIN)))
                 .spawnResource(Q(1, LEVEL))
-                .appendTrigger(new Trigger("Faluche")
+                .appendTrigger(new Trigger("Faluchard")
                 .whenReached(Q(1, FALUCHE))
                 .spawnResource(Q(1, LEVEL)))))))
         ];
--- fal/Scenario.ts
+++ fal/Scenario.ts
@@ -24,12 +24,27 @@ const LEVEL = new NamedStepResource("level", "level.svg", [
     "Faluchard"
 ]);
 const TEMPS = new Item("temps", "time.png");
-const PINS_FILIERE = new Item("pin's filière", "pins_gris.png");
-const PINS_VILLE = new Item("pin's ville", "pins.png");
 const PARRAIN = new Item("parrain", "food.svg");
 const CODE_VILLE = new Item("code de ville", "etoile_or.png");
 const FALUCHE = new Item("faluche", "faluche.png");
 
+const PINS_INGE = new Item("pin's ingé", "pins_gris.png");
+const PINS_SCIENCES = new Item("pin's sciences", "pins_gris.png");
+const PINS_ROSE = new Item("pin's rose", "pins_gris.png");
+const PINS_DROIT = new Item("pin's droit", "pins_gris.png");
+const PINS_SAGE_POUF = new Item("pin's sage-pouf", "pins_gris.png");
+const PINS_JAUNE = new Item("pin's jaune", "pins_gris.png");
+const PINS_PHARMA = new Item("pin's Pharma", "pins_gris.png");
+const PINS_MEDECINE = new Item("pin's Médecine", "pins_gris.png");
+
+const PINS_LYON = new Item("pin's Lyon", "pins.png");
+const PINS_GRENOBLE = new Item("pin's Grenoble", "pins.png");
+const PINS_MONPEUL = new Item("pin's Monpeul", "pins.png");
+const PINS_VALENCE = new Item("pin's Valence", "pins.png");
+const PINS_MARSEILLE = new Item("pin's Marseille", "pins.png");
+const PINS_DIJON = new Item("pin's Dijon", "pins.png");
+const PINS_CLERMONT = new Item("pin's Clermont", "pins.png");
+const PINS_NANCY = new Item("pin's Nancy", "pins.png");
 
 class Scenario {
     public static initEngine() : Engine {
@@ -48,37 +63,59 @@ class Scenario {
         engine.crafters = [
             // inital Crafters
             new Crafter("Apéro potes")
-                .thatCraft(new RandomRangeQuantity(0, 2, PINS_FILIERE))
+                .thatCraft(new RandomRangeQuantity(0, 2, PINS_INGE))
                 .in(2).seconds()
                 .atCostOf(Q(1, TEMPS))
         ];
 
         engine.triggers = [
             new Trigger("Sympatisant")
-                .whenReached(Q(5, PINS_FILIERE))
+                .whenReached(Q(5, PINS_INGE))
                 .spawnCrafter(
                     new Crafter("Apéro filière")
-                        .thatCraft(new RandomRangeQuantity(2, 5, PINS_FILIERE))
+                        .thatCraft(new RandomRangeQuantity(2, 5, PINS_INGE))
+                        .andCraft(new RandomResource(1, PINS_SCIENCES, 0.02))
                         .in(2).seconds()
                         .atCostOf(Q(1, TEMPS))
                 )
                 .spawnResource(Q(1, LEVEL)) // level 2
                 .appendTrigger(
                     new Trigger("Impétrent")
-                        .whenReached(Q(50, PINS_FILIERE))
+                        .whenReached(Q(50, PINS_INGE))
                         .spawnCrafter(
                             new Crafter("Apéro fal de ville")
-                                .thatCraft(new RandomRangeQuantity(1, 3, PINS_VILLE))
+                                .thatCraft(new RandomResource(1, PINS_INGE, 0.3))
+                                .thatCraft(new RandomResource(1, PINS_SCIENCES, 0.3))
+                                .thatCraft(new RandomResource(1, PINS_ROSE, 0.3))
+                                .thatCraft(new RandomResource(1, PINS_DROIT, 0.3))
+                                .thatCraft(new RandomResource(1, PINS_SAGE_POUF, 0.3))
+                                .thatCraft(new RandomResource(1, PINS_JAUNE, 0.3))
+                                .thatCraft(new RandomResource(1, PINS_PHARMA, 0.3))
+                                .thatCraft(new RandomResource(1, PINS_MEDECINE, 0.3))
                                 .in(3).seconds()
                                 .atCostOf(Q(2, TEMPS))
                         )
                         .spawnResource(Q(1, LEVEL)) // level 3
                     .appendTrigger(
                         new Trigger("Néo")
-                            .whenReached(Q(20, PINS_VILLE))
+                            .whenReached(Q(1, PINS_INGE))
+                            .whenReached(Q(1, PINS_SCIENCES))
+                            .whenReached(Q(1, PINS_ROSE))
+                            .whenReached(Q(1, PINS_DROIT))
+                            .whenReached(Q(1, PINS_SAGE_POUF))
+                            .whenReached(Q(1, PINS_JAUNE))
+                            .whenReached(Q(1, PINS_PHARMA))
+                            .whenReached(Q(1, PINS_MEDECINE))
                             .spawnCrafter(
                                 new Crafter("Apéro fal hebdomadaire")
-                                    .thatCraft(Q(3, PINS_VILLE))
+                                    .thatCraft(new RandomResource(1, PINS_INGE, 0.4))
+                                    .thatCraft(new RandomResource(1, PINS_SCIENCES, 0.4))
+                                    .thatCraft(new RandomResource(1, PINS_DROIT, 0.4))
+                                    .thatCraft(new RandomResource(1, PINS_JAUNE, 0.4))
+                                    .thatCraft(new RandomResource(1, PINS_ROSE, 0.3))
+                                    .thatCraft(new RandomResource(1, PINS_SAGE_POUF, 0.3))
+                                    .thatCraft(new RandomResource(1, PINS_PHARMA, 0.3))
+                                    .thatCraft(new RandomResource(1, PINS_MEDECINE, 0.3))
                                     .in(3).seconds()
                                     .atCostOf(Q(2, TEMPS))
                                     .canBeSwitchedToAuto()
@@ -87,7 +124,14 @@ class Scenario {
                                 new Crafter("Trouver des parrains marraines")
                                     .thatCraft(Q(1, PARRAIN))
                                     .in(20).seconds()
-                                    .atCostOf(Q(30, PINS_VILLE)).and(Q(30, PINS_FILIERE))
+                                    .atCostOf(Q(8, PINS_INGE))
+                                    .atCostOf(Q(6, PINS_SCIENCES))
+                                    .atCostOf(Q(6, PINS_DROIT))
+                                    .atCostOf(Q(6, PINS_JAUNE))
+                                    .atCostOf(Q(6, PINS_ROSE))
+                                    .atCostOf(Q(6, PINS_SAGE_POUF))
+                                    .atCostOf(Q(6, PINS_PHARMA))
+                                    .atCostOf(Q(6, PINS_MEDECINE))
                             )
                             .spawnResource(Q(1, LEVEL)) // level 4
                             .appendTrigger(
@@ -97,7 +141,14 @@ class Scenario {
                                         new Crafter("Apprentissage du code")
                                             .thatCraft(Q(1, CODE_VILLE))
                                             .in(30).seconds()
-                                            .atCostOf(Q(20, PINS_VILLE))
+                                            .atCostOf(Q(4, PINS_INGE))
+                                            .atCostOf(Q(4, PINS_SCIENCES))
+                                            .atCostOf(Q(4, PINS_DROIT))
+                                            .atCostOf(Q(4, PINS_JAUNE))
+                                            .atCostOf(Q(4, PINS_ROSE))
+                                            .atCostOf(Q(4, PINS_SAGE_POUF))
+                                            .atCostOf(Q(4, PINS_PHARMA))
+                                            .atCostOf(Q(4, PINS_MEDECINE))
                                     )
                                     .spawnResource(Q(1, LEVEL)) // level 5
                                     .appendTrigger(
@@ -105,13 +156,13 @@ class Scenario {
                                             .whenReached(Q(1, CODE_VILLE))
                                             .spawnCrafter(
                                                 new Crafter("Baptême")
-                                                    .thatCraft(Q(1, FALUCHE))
+                                                    .thatCraft(Q(1, FALUCHE)).andCraft(Q(1, PINS_GRENOBLE))
                                                     .in(30).seconds()
                                                     .atCostOf(Q(1, CODE_VILLE)).and(Q(2, PARRAIN))
                                             )
                                             .spawnResource(Q(1, LEVEL)) // level 6
                                             .appendTrigger(
-                                                new Trigger("Faluche")
+                                                new Trigger("Faluchard")
                                                     .whenReached(Q(1, FALUCHE))
                                                     .spawnResource(Q(1, LEVEL)) // level 7
                                             )
