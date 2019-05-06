aea43b13f5202be7ad16f5abc9ee3c50e3ca9ae8
Yannick Battail <yannick.battail@raisepartner.com>
2019-05-06 11:22:45 +0200

    ajout soirée fal et pin's natio

--- fal/Scenario.js
+++ fal/Scenario.js
@@ -6,11 +6,13 @@ var LEVEL = new NamedStepResource("level", "level.svg", [
     "Néo",
     "Parrainé",
     "Baptisable",
+    "Bébé fal",
     "Faluchard"
 ]);
 var TEMPS = new Item("temps", "time.png");
 var PARRAIN = new Item("parrain", "food.svg");
 var CODE_VILLE = new Item("code de ville", "etoile_or.png");
+var POULE = new Item("poule", "poule.png");
 var FALUCHE = new Item("adoption", "faluche.png");
 var ADOPTION_INGE = new Item("adoption ingé", "faluche.png");
 var ADOPTION_SCIENCES = new Item("adoption sciences", "faluche.png");
@@ -29,13 +31,13 @@ var PINS_SAGE_POUF = new Item("pin's sage-pouf", "pins_gris.png");
 var PINS_PHARMA = new Item("pin's Pharma", "pins_gris.png");
 var PINS_MEDECINE = new Item("pin's Médecine", "pins_gris.png");
 var PINS_GRENOBLE = new Item("pin's Grenoble", "pins.png");
-var PINS_STASBOURG = new Item("pin's Strasbourg", "pins.png");
-var PINS_MONPEUL = new Item("pin's Monpeul", "pins.png");
 var PINS_VALENCE = new Item("pin's Valence", "pins.png");
-var PINS_MARSEILLE = new Item("pin's Marseille", "pins.png");
-var PINS_DIJON = new Item("pin's Dijon", "pins.png");
 var PINS_CLERMONT = new Item("pin's Clermont", "pins.png");
+var PINS_DIJON = new Item("pin's Dijon", "pins.png");
+var PINS_MONPEUL = new Item("pin's Monpeul", "pins.png");
+var PINS_MARSEILLE = new Item("pin's Marseille", "pins.png");
 var PINS_NANCY = new Item("pin's Nancy", "pins.png");
+var PINS_STASBOURG = new Item("pin's Strasbourg", "pins.png");
 var Q = function (quantity, res) { return new Quantity(quantity, res); };
 var Scenario = (function () {
     function Scenario() {
@@ -122,11 +124,9 @@ var Scenario = (function () {
                 .atCostOf(Q(4, PINS_PHARMA))
                 .atCostOf(Q(4, PINS_MEDECINE)))
                 .spawnResource(Q(1, LEVEL))
-                .appendTrigger(new Trigger("Baptisable")
-                .whenReached(Q(1, CODE_VILLE))
-                .spawnCrafter(new Crafter("Baptême")
-                .thatCraft(Q(1, FALUCHE)).andCraft(Q(1, PINS_GRENOBLE))["in"](30).seconds()
-                .atCostOf(Q(1, CODE_VILLE)).and(Q(2, PARRAIN)))
+                .appendTrigger(new Trigger("Baptême")
+                .whenReached(Q(1, CODE_VILLE)).and(Q(2, PARRAIN))
+                .spawnResource(Q(1, FALUCHE))
                 .spawnResource(Q(1, LEVEL))
                 .appendTrigger(Scenario.triggerFal())))))
         ];
@@ -136,31 +136,60 @@ var Scenario = (function () {
         return new Trigger("Faluchard")
             .whenReached(Q(1, FALUCHE))
             .spawnResource(Q(1, LEVEL))
+            .spawnResource(Q(0, PINS_GRENOBLE))
+            .spawnResource(Q(0, PINS_VALENCE))
+            .spawnResource(Q(0, PINS_CLERMONT))
+            .spawnResource(Q(0, PINS_DIJON))
+            .spawnResource(Q(0, PINS_MONPEUL))
+            .spawnResource(Q(0, PINS_MARSEILLE))
+            .spawnResource(Q(0, PINS_NANCY))
+            .spawnResource(Q(0, PINS_STASBOURG))
             .spawnProducer(new Producer("Temps / jours")
             .thatProduce(Q(10, TEMPS))
             .every(30).seconds())
-            .spawnCrafter(new Crafter("Adoption sciences")
-            .thatCraft(Q(1, ADOPTION_SCIENCES))["in"](30).seconds()
-            .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_SCIENCES)))
-            .spawnCrafter(new Crafter("Adoption droit")
-            .thatCraft(Q(1, ADOPTION_DROIT))["in"](30).seconds()
-            .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_DROIT)))
-            .spawnCrafter(new Crafter("Adoption jaune")
-            .thatCraft(Q(1, ADOPTION_JAUNE))["in"](30).seconds()
-            .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_JAUNE)))
-            .spawnCrafter(new Crafter("Adoption rose")
-            .thatCraft(Q(1, ADOPTION_ROSE))["in"](30).seconds()
-            .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_ROSE)))
-            .spawnCrafter(new Crafter("Adoption sage-pouf")
-            .thatCraft(Q(1, ADOPTION_SAGE_POUF))["in"](30).seconds()
-            .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_SAGE_POUF)))
-            .spawnCrafter(new Crafter("Adoption pharma")
-            .thatCraft(Q(1, ADOPTION_PHARMA))["in"](30).seconds()
-            .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_PHARMA)))
-            .spawnCrafter(new Crafter("Adoption médecine")
-            .thatCraft(Q(1, ADOPTION_MEDECINE))["in"](30).seconds()
-            .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_MEDECINE)))
-            .appendTrigger(new Trigger("pu** à adoption")
+            .spawnCrafter(new Crafter("Soirée fal")
+            .thatCraft(new RandomResource(1, PINS_GRENOBLE, 100 / 113))
+            .thatCraft(new RandomResource(1, PINS_VALENCE, 100 / 104))
+            .thatCraft(new RandomResource(1, PINS_CLERMONT, 100 / 165))
+            .thatCraft(new RandomResource(1, PINS_DIJON, 100 / 197))
+            .thatCraft(new RandomResource(1, PINS_MONPEUL, 100 / 306))
+            .thatCraft(new RandomResource(1, PINS_MARSEILLE, 100 / 315))
+            .thatCraft(new RandomResource(1, PINS_NANCY, 100 / 407))
+            .thatCraft(new RandomResource(1, PINS_STASBOURG, 100 / 492))["in"](3).seconds()
+            .atCostOf(Q(5, TEMPS)))
+            .appendTrigger(new Trigger("Natio")
+            .whenReached(Q(1, PINS_GRENOBLE))
+            .whenReached(Q(1, PINS_VALENCE))
+            .whenReached(Q(1, PINS_CLERMONT))
+            .whenReached(Q(1, PINS_DIJON))
+            .whenReached(Q(1, PINS_MONPEUL))
+            .whenReached(Q(1, PINS_MARSEILLE))
+            .whenReached(Q(1, PINS_NANCY))
+            .whenReached(Q(1, PINS_STASBOURG))
+            .spawnResource(Q(1, LEVEL)))
+            .appendTrigger(new Trigger("[secondaire] Adoption sciences")
+            .whenReached(Q(1, CODE_VILLE))
+            .whenReached(Q(1, CODE_VILLE))
+            .spawnResource(Q(1, ADOPTION_SCIENCES)))
+            .appendTrigger(new Trigger("[secondaire] Adoption droit")
+            .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_DROIT))
+            .spawnResource(Q(1, ADOPTION_DROIT)))
+            .appendTrigger(new Trigger("[secondaire] Adoption jaune")
+            .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_JAUNE))
+            .spawnResource(Q(1, ADOPTION_JAUNE)))
+            .appendTrigger(new Trigger("[secondaire] Adoption rose")
+            .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_ROSE))
+            .spawnResource(Q(1, ADOPTION_ROSE)))
+            .appendTrigger(new Trigger("[secondaire] Adoption sage-pouf")
+            .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_SAGE_POUF))
+            .spawnResource(Q(1, ADOPTION_SAGE_POUF)))
+            .appendTrigger(new Trigger("[secondaire] Adoption pharma")
+            .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_PHARMA))
+            .spawnResource(Q(1, ADOPTION_PHARMA)))
+            .appendTrigger(new Trigger("[secondaire] Adoption médecine")
+            .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_MEDECINE))
+            .spawnResource(Q(1, ADOPTION_MEDECINE)))
+            .appendTrigger(new Trigger("[secondaire] p*te à adoption")
             .whenReached(Q(1, ADOPTION_SCIENCES))
             .and(Q(1, ADOPTION_DROIT))
             .and(Q(1, ADOPTION_JAUNE))
@@ -168,7 +197,7 @@ var Scenario = (function () {
             .and(Q(1, ADOPTION_SAGE_POUF))
             .and(Q(1, ADOPTION_PHARMA))
             .and(Q(1, ADOPTION_MEDECINE))
-            .spawnResource(Q(1, LEVEL)));
+            .spawnResource(Q(1, POULE)));
     };
     return Scenario;
 }());
--- fal/Scenario.ts
+++ fal/Scenario.ts
@@ -21,11 +21,13 @@ const LEVEL = new NamedStepResource("level", "level.svg", [
     "Néo",
     "Parrainé",
     "Baptisable",
+    "Bébé fal",
     "Faluchard"
 ]);
 const TEMPS = new Item("temps", "time.png");
 const PARRAIN = new Item("parrain", "food.svg");
 const CODE_VILLE = new Item("code de ville", "etoile_or.png");
+const POULE = new Item("poule", "poule.png");
 
 const FALUCHE = new Item("adoption", "faluche.png");
 
@@ -47,14 +49,14 @@ const PINS_SAGE_POUF = new Item("pin's sage-pouf", "pins_gris.png");
 const PINS_PHARMA = new Item("pin's Pharma", "pins_gris.png");
 const PINS_MEDECINE = new Item("pin's Médecine", "pins_gris.png");
 
-const PINS_GRENOBLE = new Item("pin's Grenoble", "pins.png");
-const PINS_STASBOURG = new Item("pin's Strasbourg", "pins.png");
-const PINS_MONPEUL = new Item("pin's Monpeul", "pins.png");
-const PINS_VALENCE = new Item("pin's Valence", "pins.png");
-const PINS_MARSEILLE = new Item("pin's Marseille", "pins.png");
-const PINS_DIJON = new Item("pin's Dijon", "pins.png");
-const PINS_CLERMONT = new Item("pin's Clermont", "pins.png");
-const PINS_NANCY = new Item("pin's Nancy", "pins.png");
+const PINS_GRENOBLE = new Item("pin's Grenoble", "pins.png"); // 113km
+const PINS_VALENCE = new Item("pin's Valence", "pins.png"); // 104km
+const PINS_CLERMONT = new Item("pin's Clermont", "pins.png"); // 165km
+const PINS_DIJON = new Item("pin's Dijon", "pins.png"); // 197km
+const PINS_MONPEUL = new Item("pin's Monpeul", "pins.png"); // 306km
+const PINS_MARSEILLE = new Item("pin's Marseille", "pins.png"); // 315km
+const PINS_NANCY = new Item("pin's Nancy", "pins.png"); // 407km
+const PINS_STASBOURG = new Item("pin's Strasbourg", "pins.png"); // 492km
 
 
 let Q = (quantity : number, res : IResource) => new Quantity(quantity, res);
@@ -169,14 +171,9 @@ class Scenario {
                                         )
                                         .spawnResource(Q(1, LEVEL)) // level 5
                                         .appendTrigger(
-                                            new Trigger("Baptisable")
-                                                .whenReached(Q(1, CODE_VILLE))
-                                                .spawnCrafter(
-                                                    new Crafter("Baptême")
-                                                        .thatCraft(Q(1, FALUCHE)).andCraft(Q(1, PINS_GRENOBLE))
-                                                        .in(30).seconds()
-                                                        .atCostOf(Q(1, CODE_VILLE)).and(Q(2, PARRAIN))
-                                                )
+                                            new Trigger("Baptême")
+                                                .whenReached(Q(1, CODE_VILLE)).and(Q(2, PARRAIN))
+                                                .spawnResource(Q(1, FALUCHE))
                                                 .spawnResource(Q(1, LEVEL)) // level 6
                                                 .appendTrigger(Scenario.triggerFal()) // level 7
                                         )
@@ -191,55 +188,83 @@ class Scenario {
         return new Trigger("Faluchard")
             .whenReached(Q(1, FALUCHE))
             .spawnResource(Q(1, LEVEL)) // level 7
+            .spawnResource(Q(0, PINS_GRENOBLE))
+            .spawnResource(Q(0, PINS_VALENCE))
+            .spawnResource(Q(0, PINS_CLERMONT))
+            .spawnResource(Q(0, PINS_DIJON))
+            .spawnResource(Q(0, PINS_MONPEUL))
+            .spawnResource(Q(0, PINS_MARSEILLE))
+            .spawnResource(Q(0, PINS_NANCY))
+            .spawnResource(Q(0, PINS_STASBOURG))
             .spawnProducer(
                 new Producer("Temps / jours")
                     .thatProduce(Q(10, TEMPS))
                     .every(30).seconds()
             )
             .spawnCrafter(
-                new Crafter("Adoption sciences")
-                    .thatCraft(Q(1, ADOPTION_SCIENCES))
-                    .in(30).seconds()
-                    .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_SCIENCES))
-            )
-            .spawnCrafter(
-                new Crafter("Adoption droit")
-                    .thatCraft(Q(1, ADOPTION_DROIT))
-                    .in(30).seconds()
-                    .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_DROIT))
-            )
-            .spawnCrafter(
-                new Crafter("Adoption jaune")
-                    .thatCraft(Q(1, ADOPTION_JAUNE))
-                    .in(30).seconds()
-                    .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_JAUNE))
-            )
-            .spawnCrafter(
-                new Crafter("Adoption rose")
-                    .thatCraft(Q(1, ADOPTION_ROSE))
-                    .in(30).seconds()
-                    .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_ROSE))
-            )
-            .spawnCrafter(
-                new Crafter("Adoption sage-pouf")
-                    .thatCraft(Q(1, ADOPTION_SAGE_POUF))
-                    .in(30).seconds()
-                    .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_SAGE_POUF))
-            )
-            .spawnCrafter(
-                new Crafter("Adoption pharma")
-                    .thatCraft(Q(1, ADOPTION_PHARMA))
-                    .in(30).seconds()
-                    .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_PHARMA))
-            )
-            .spawnCrafter(
-                new Crafter("Adoption médecine")
-                    .thatCraft(Q(1, ADOPTION_MEDECINE))
-                    .in(30).seconds()
-                    .atCostOf(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_MEDECINE))
+                new Crafter("Soirée fal")
+                    .thatCraft(new RandomResource(1, PINS_GRENOBLE, 100/113))
+                    .thatCraft(new RandomResource(1, PINS_VALENCE, 100/104))
+                    .thatCraft(new RandomResource(1, PINS_CLERMONT, 100/165))
+                    .thatCraft(new RandomResource(1, PINS_DIJON, 100/197))
+                    .thatCraft(new RandomResource(1, PINS_MONPEUL, 100/306))
+                    .thatCraft(new RandomResource(1, PINS_MARSEILLE, 100/315))
+                    .thatCraft(new RandomResource(1, PINS_NANCY, 100/407))
+                    .thatCraft(new RandomResource(1, PINS_STASBOURG, 100/492))
+                    .in(3).seconds()
+                    .atCostOf(Q(5, TEMPS))  
             )
             .appendTrigger(
-                new Trigger("pu** à adoption")
+                new Trigger("Natio")
+                    .whenReached(Q(1, PINS_GRENOBLE))
+                    .whenReached(Q(1, PINS_VALENCE))
+                    .whenReached(Q(1, PINS_CLERMONT))
+                    .whenReached(Q(1, PINS_DIJON))
+                    .whenReached(Q(1, PINS_MONPEUL))
+                    .whenReached(Q(1, PINS_MARSEILLE))
+                    .whenReached(Q(1, PINS_NANCY))
+                    .whenReached(Q(1, PINS_STASBOURG))
+                    .spawnResource(Q(1, LEVEL)) // level 8  
+            )
+            .appendTrigger(
+                new Trigger("[secondaire] Adoption sciences")
+                    .whenReached(Q(1, CODE_VILLE))
+                    .whenReached(Q(1, CODE_VILLE))
+
+                    .spawnResource(Q(1, ADOPTION_SCIENCES))
+            )
+            .appendTrigger(
+                new Trigger("[secondaire] Adoption droit")
+                    .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_DROIT))
+                    .spawnResource(Q(1, ADOPTION_DROIT))
+            )
+            .appendTrigger(
+                new Trigger("[secondaire] Adoption jaune")
+                    .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_JAUNE))
+                    .spawnResource(Q(1, ADOPTION_JAUNE))
+            )
+            .appendTrigger(
+                new Trigger("[secondaire] Adoption rose")
+                    .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_ROSE))
+                    .spawnResource(Q(1, ADOPTION_ROSE))
+            )
+            .appendTrigger(
+                new Trigger("[secondaire] Adoption sage-pouf")
+                    .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_SAGE_POUF))
+                    .spawnResource(Q(1, ADOPTION_SAGE_POUF))
+            )
+            .appendTrigger(
+                new Trigger("[secondaire] Adoption pharma")
+                    .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_PHARMA))
+                    .spawnResource(Q(1, ADOPTION_PHARMA))
+            )
+            .appendTrigger(
+                new Trigger("[secondaire] Adoption médecine")
+                    .whenReached(Q(1, CODE_VILLE)).and(Q(1, PARRAIN)).and(Q(40, PINS_MEDECINE))
+                    .spawnResource(Q(1, ADOPTION_MEDECINE))
+            )
+            .appendTrigger(
+                new Trigger("[secondaire] p*te à adoption")
                     .whenReached(Q(1, ADOPTION_SCIENCES))
                     .and(Q(1, ADOPTION_DROIT))
                     .and(Q(1, ADOPTION_JAUNE))
@@ -247,7 +272,7 @@ class Scenario {
                     .and(Q(1, ADOPTION_SAGE_POUF))
                     .and(Q(1, ADOPTION_PHARMA))
                     .and(Q(1, ADOPTION_MEDECINE))
-                    .spawnResource(Q(1, LEVEL)) // level 6
+                    .spawnResource(Q(1, POULE))
 
             );
     }
