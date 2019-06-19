ea12b132e2596fe204ba7802bc66d61e4e9a1f06
Yannick Battail <yannick.battail@raisepartner.com>
2019-06-19 11:04:11 +0200

    add script that create zip release

---
+++ fal/zip_release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+rm -Rf temp_dir
+mkdir temp_dir
+
+mkdir temp_dir/fal
+cp index.html temp_dir/fal/
+cp *.js temp_dir/fal/
+cp -R images temp_dir/fal/
+cp -R styles temp_dir/fal/
+
+cp ../json3.min.js temp_dir/
+
+mkdir temp_dir/Engine
+cp ../Engine/*.js temp_dir/Engine/
+mkdir temp_dir/Engine/implementations
+cp ../Engine/implementations/*.js temp_dir/Engine/implementations/
+
+mkdir temp_dir/NodeUpdate
+cp ../NodeUpdate/NodeUpdate.js temp_dir/NodeUpdate/
+
+echo "open file fal/index.html" >> temp_dir/readme.txt
+echo "sources can be found here https://github.com/yannickbattail/incrementum-ludus" >> temp_dir/readme.txt
+
+cd temp_dir
+7z a ../incrementFal.zip ./
+cd ..
+
+rm -Rf temp_dir
